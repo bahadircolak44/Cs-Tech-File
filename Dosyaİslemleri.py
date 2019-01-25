d1fc8eed37b1da088ae1f48f082595f2cf4d958a
bahadircolak44 <bahdrcolak@gmail.com>
2019-01-25 14:28:40 +0300

    First init

---
+++ Dosyaİslemleri.py
@@ -0,0 +1,26 @@
+import re
+
+def f_komutu(kelime):
+    
+    # aratilacak kelimede - karakteri var is regex icin . ile degistir. 
+    if "-" in kelime:
+        kelime = kelime.replace("-",".")
+        result = re.findall(kelime,f.read())
+        print(len(result)," tane eslesme bulundu.")
+        print (result)
+        
+    #"-" veya "*" icermiyorsa
+    else:
+        i = 0
+        for x in f:
+            for word in x.split():
+                if kelime == word:
+                    i = i + 1
+        print (i," tane eslesme bulundu.")
+try:              
+    f = open('input.txt','r')
+    f_komutu("fazla")
+except IOError:
+    print ("Dosya bulunamadi.")
+    
+
