7a466cb752ec0dae1ded320a63010dcd61291169
bahadircolak44 <bahdrcolak@gmail.com>
2019-01-26 20:22:20 -0800

    Bazi düzeltmeler

--- Dosyaİslemleri.py
+++ Dosyaİslemleri.py
@@ -1,37 +1,43 @@
 import re
 
-def f_komutu(kelime,fileName):
+def f_komutu(text,fileName):
     f = open(fileName,'r')
+    i = 0
     # aratilacak kelimede - karakteri var is regex icin . ile degistir. 
-    if "-" in kelime:
-        kelime = kelime.replace('-','.')
-        result = re.findall(kelime,f.read())
-        print(len(result),' tane eslesme bulundu.')
-
-    # kelimenin basindaki ve sonunda ki * lar silinip textin icindeki kelimelerde bu substringi içeren olup olmadigini arar.
-    elif "*" in kelime:
-        kelime = kelime.replace('*','')
+    if "-" in text:
+        text = text.replace('-','.')
         for x in f:
             for word in x.split():
-                if kelime in word:
+                if re.fullmatch(text,word):
+                    i = i+1
                     print(word)
-                    
+        print(i,' tane eslesme bulundu.')
+
+    # kelimenin basindaki ve sonunda ki * lar silinip textin icindeki kelimelerde bu substringi içeren olup olmadigini arar.
+    elif "*" in text:
+        text = text.replace('*','[a-zA-Z]+')
+        for x in f:
+            for word in x.split():
+                if re.fullmatch(text, word):
+                    i = i + 1
+                    print(word)
+
     #"-" veya "*" icermiyorsa
     else:
         i = 0
         for x in f:
             for word in x.split():
-                if kelime == word:
+                if text == word:
                     i = i + 1
         print (i,' tane eslesme bulundu.')
 
-def r_komutu(word1,word2,fileName):
+def r_komutu(text1,text2,fileName):
     f = open(fileName, 'r')
     s = ' '
     for x in f:
         for word in x.split():
-            if word == word1:
-                word = word2
+            if word == text1:
+                word = text2
                 s = s+word + ' '
             else:
                 s = s + word + ' '
@@ -39,22 +45,26 @@ def r_komutu(word1,word2,fileName):
     with open(fileName, "w") as f:
         f.write(s)
 def d_komutu(text,fileName):
-    f = open(fileName, 'r')
-    s = ' '
-    for x in f:
-        for word in x.split():
-            if word == text:
-                continue
-            else:
-                s = s + word + ' '
+    try:
+        f = open(fileName, 'r')
+        s = ' '
+        for x in f:
+            for word in x.split():
+                if word == text:
+                    continue
+                else:
+                    s = s + word + ' '
 
-    with open(fileName, "w") as f:
-        f.write(s)
-try:
-    fileName = 'input.txt'
-    #r_komutu('a', '1', fileName)
-    d_komutu('ilk',fileName)
-except IOError:
-    print('Dosya bulunamadi.')
+        with open(fileName, "w") as f:
+            f.write(s)
+
+    except IOError:
+        print('Dosya bulunamadi.')
+
+fileName = 'input.txt'
+f_komutu('*an',fileName)
+#r_komutu('a', '1', fileName)
+#d_komutu('ilk',fileName)
+
     
 
